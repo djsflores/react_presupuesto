57ef7dfe29320593e0bb9c3a33b858d0eee7353e
David <david.jesus.sebastian.flores@gmail.com>
2021-08-17 20:59:49 -0300

    creamos un state para los gastos, validamos los gastos y creamos un gasto

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,15 +1,55 @@
 import React, {useState} from 'react';
+import Error from './Error';
+import shortid from 'shortid';
 
 const Formulario = () => {
+
+    const [ nombre, guardarNombre ] = useState('');
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, guardarError ] = useState(false);
+
+    // Cuando el usuario agrega un gasto
+    const agregarGasto = e => {
+        e.preventDefault();
+
+        // validar
+        if( cantidad < 1 || isNaN(cantidad) || nombre.trim === '' ){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        // construir el gasto
+        const gasto = {
+            nombre,
+            cantidad,
+            id: shortid.generate()
+        }
+
+        console.log(gasto);
+
+        // pasar el gasto al componente principal
+
+        // resetear el form
+
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={agregarGasto}
+        >
             <h2>Agrega tus gastos aquí</h2>
+
+            { error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto incorrecto" /> : null}
+
             <div className="campo">
                 <label>Nombre gasto</label>
                 <input
                     type="text"
                     className="u-full-width"
                     placeholder="Ej. Transporte"
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
                 />
             </div>
             <div className="campo">
@@ -18,6 +58,8 @@ const Formulario = () => {
                     type="number"
                     className="u-full-width"
                     placeholder="Ej. 300"
+                    value={cantidad}
+                    onChange={e => guardarCantidad( parseInt( e.target.value, 10 ))}
                 />
             </div>
             <input
